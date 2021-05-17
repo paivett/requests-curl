986dd0e1d7e7bfed8df28f1f9e36d4a3eebd2427
paivett <santiago.pivetta@gmail.com>
2021-05-16 21:54:56 -0300

    Add some e2e cookies tests

---
+++ tests_e2e/test_cookies.py
@@ -0,0 +1,49 @@
+import requests
+
+from requests_curl import CURLAdapter
+
+
+def test_empty_cookies_when_no_cookies_were_set():
+    session = requests.Session()
+
+    session.mount("http://", CURLAdapter())
+
+    url = f"http://http_bin/cookies"
+
+    response = session.get(url)
+
+    assert not response.cookies
+
+
+def test_set_single_cookie():
+    session = requests.Session()
+
+    session.mount("http://", CURLAdapter())
+    
+    url = "http://http_bin/cookies/set/foo/cookievalue"
+
+    response = session.get(url)
+
+    assert session.cookies["foo"] == "cookievalue"
+
+    cookies = response.json()["cookies"]
+    assert cookies == {"foo": "cookievalue"}
+
+
+def test_delete_cookies():
+    session = requests.Session()
+
+    session.mount("http://", CURLAdapter())
+    
+    url = "http://http_bin/cookies/set/foo/cookievalue"
+
+    response = session.get(url)
+
+    assert session.cookies["foo"] == "cookievalue"
+
+    session.cookies.clear()
+
+    response = session.get("http://http_bin/cookies")
+
+    assert response.json()["cookies"] == {}
+
